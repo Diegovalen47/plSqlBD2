5324aadda656caf42abb8217690c5ee45f140dac
Diegovalen47 <osoriovalentin5@gmail.com>
2022-09-06 08:12:25 -0500

    Creacion de tablas

---
+++ tables_creation.sql
@@ -0,0 +1,20 @@
+-- Creacion de tablas
+
+CREATE TABLE cooperativa (
+  codigo NUMBER(8) PRIMARY KEY,
+  nombre VARCHAR2(30) NOT NULL UNIQUE,
+  c_acumulado NUMBER(8)
+);
+
+CREATE TABLE socio (
+  idsocio NUMBER(8) PRIMARY KEY,
+  nombre VARCHAR2(30) NOT NULL,
+  s_acumulado NUMBER(11,3) CHECK(s_acumulado >= 0)
+);
+
+CREATE TABLE coopexsocio (
+  socio NUMBER(8) REFERENCES socio,
+  coope NUMBER(11, 3) REFERENCES cooperativa,
+  PRIMARY KEY(socio, coope),
+  sc_acumulado NUMBER(8)
+);
