a8739bb567955674396a00dd1b14725b50d3100b
Jan Michael Muñoz Botero <jmunozbo@unal.edu.co>
2022-09-10 18:44:13 -0500

    Programa 1 - 20%

---
+++ firstProgram.sql
@@ -0,0 +1,36 @@
+CREATE PROCEDURE programa1
+(codigo_cooperativa IN COOPERATIVA.CODIGO%TYPE) IS
+    nombre_cooperativa COOPERATIVA.NOMBRE%TYPE;
+    acumulado_cooperativa COOPERATIVA.C_ACUMULADO%TYPE;
+    cantidad_socios NUMBER(8);
+    CURSOR socios_programa_1 IS SELECT * from SOCIO INNER JOIN COOPEXSOCIO ON SOCIO.IDSOCIO=COOPEXSOCIO.SOCIO;
+    contador_impresor NUMBER(8):=1;
+BEGIN
+    SELECT NOMBRE, C_ACUMULADO INTO nombre_cooperativa,acumulado_cooperativa  FROM COOPERATIVA WHERE CODIGO=codigo_cooperativa;
+    DBMS_OUTPUT.PUT_LINE('Nombre de la cooperativa: '||nombre_cooperativa);
+    IF(acumulado_cooperativa IS NULL) THEN
+        DBMS_OUTPUT.PUT_LINE('Acumulado de la cooperativa: '||'0');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Acumulado de la cooperativa: '||acumulado_cooperativa);
+    END IF;
+    SELECT count(*) INTO cantidad_socios FROM COOPEXSOCIO WHERE COOPE=codigo_cooperativa;
+    DBMS_OUTPUT.PUT_LINE('Número de socios: '||cantidad_socios);
+    FOR recorredor IN socios_programa_1 LOOP
+        IF (recorredor.COOPE=codigo_cooperativa) THEN
+            IF (recorredor.SC_ACUMULADO IS NULL) THEN
+                DBMS_OUTPUT.PUT_LINE(contador_impresor||'. (Nombre: '||recorredor.NOMBRE||', Valorsc: '||'0'||')');
+            ELSE
+                DBMS_OUTPUT.PUT_LINE(contador_impresor||'. (Nombre: '||recorredor.NOMBRE||', Valorsc: '||recorredor.SC_ACUMULADO||')');
+            END IF;
+            contador_impresor:=contador_impresor+1;
+        END IF;
+        END LOOP;
+
+END;
+/
+
+
+
+
+
+
